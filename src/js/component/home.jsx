3e8b4e1bc79d48c021066bea7922264066839d21
JackCaldwellNichols <jack.wcn@gmail.com>
2022-10-17 12:44:30 +0000

    my first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+
+	//Statehook section
+	const [task, setTask] = useState('');
+	const [items, setItems] = useState([]);
+
+
+	//helper funtions
+	const handleKeyDown = (event) => {
+		if (event.key === "Enter") {
+		  addTask();
+		}
+	  };
+
+	function addTask(){
+
+		if(!task){
+			alert("You must have something to do!")
+		}else{
+
+		const item = {
+			id: Math.floor(Math.random()*1000),
+			value: task
+		}
+
+		setItems(oldList => [...oldList, item]) 
+		setTask("");
+	}}
+
+	function deleteItems(id) {
+		const newArray = items.filter(item => item.id !== id)
+		setItems(newArray);
+	}
+	   return (
+			<div className="container">
+					
+				<h1>ToDo List</h1>
+			
+
+				
+					<ul>
+					<input className = "input" type="text" placeholder="enter your task..." value ={task} onChange={e => setTask(e.target.value)} onKeyDown={handleKeyDown}></input>
+					
+						{items.map(item => {
+							return (
+								<li key={item.id}>{item.value}<button className="delete" onClick={() => deleteItems(item.id)}><FontAwesomeIcon icon={faTrashCan}> </FontAwesomeIcon></button></li>					
+							)
+						})}
+					</ul>
+				
+			</div>
+
+	   )
+	};
+
 
 export default Home;
