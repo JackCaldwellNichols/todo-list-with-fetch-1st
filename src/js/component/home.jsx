5effd0b810c2371207817781d9b8133800d4931e
JackCaldwellNichols <jack.wcn@gmail.com>
2022-10-25 19:43:06 +0000

    to be corrected

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 //create your first component
@@ -10,6 +10,41 @@ const Home = () => {
 	//Statehook section
 	const [task, setTask] = useState('');
 	const [items, setItems] = useState([]);
+
+	const getList = (items) => {
+		var requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+		  };
+		  
+		  fetch("https://assets.breatheco.de/apis/fake/todos/user/jack1987", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));		
+	};
+
+	const updateList = () => {
+
+		var requestOptions = {
+			method: 'PUT',
+			body: JSON.stringify(items),
+			redirect: 'follow'
+		  };
+		  
+		  fetch("https://assets.breatheco.de/apis/fake/todos/user/jack1987", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
+		}
+
+	useEffect(() => {
+		getList(items);
+	}, []);
+
+
+	useEffect(() => {
+		updateList();
+	},[addTask]); 
 
 
 	//helper funtions
@@ -20,7 +55,6 @@ const Home = () => {
 	  };
 
 	function addTask(){
-
 		if(!task){
 			alert("You must have something to do!")
 		}else{
@@ -29,7 +63,6 @@ const Home = () => {
 			id: Math.floor(Math.random()*1000),
 			value: task
 		}
-
 		setItems(oldList => [...oldList, item]) 
 		setTask("");
 	}}
@@ -38,27 +71,23 @@ const Home = () => {
 		const newArray = items.filter(item => item.id !== id)
 		setItems(newArray);
 	}
-	   return (
-			<div className="container">
-					
-				<h1>ToDo List</h1>
-			
 
-				
+
+
+	   return (
+			<div className="container">					
+				<h1>ToDo List</h1>
 					<ul>
 					<input className = "input" type="text" placeholder="enter your task..." value ={task} onChange={e => setTask(e.target.value)} onKeyDown={handleKeyDown}></input>
-					
 						{items.map(item => {
 							return (
-								<li key={item.id}>{item.value}<button className="delete" onClick={() => deleteItems(item.id)}><FontAwesomeIcon icon={faTrashCan}> </FontAwesomeIcon></button></li>					
+								<li key={item.id}>{item.value}<button className="delete" onClick={() => deleteItems(item.id)}><FontAwesomeIcon icon={faTrashCan}> </FontAwesomeIcon></button></li>		
 							)
 						})}
+								<li>{items.length + " items left"}</li>
 					</ul>
-				
 			</div>
-
 	   )
 	};
-
 
 export default Home;
